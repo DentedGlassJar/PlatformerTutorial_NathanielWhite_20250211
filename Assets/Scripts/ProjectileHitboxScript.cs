96f9a604c2d6bae9a06ed7fb96ee661cd199f1cb
DentedGlassJar <nathanielwhite867991@gmail.com>
2025-03-07 23:21:48 -0400

    Now I'm Done

--- Assets/Scripts/ProjectileHitboxScript.cs
+++ Assets/Scripts/ProjectileHitboxScript.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class ProjectileHitboxScript : MonoBehaviour
 {
     public GameObject enemyObj;
-    // Used to check if the collider other enters the trigger
 
+    // Used to check if the collider other enters the trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        // If the projectile hits the enemy, it takes damage
+        if (other.gameObject.CompareTag("Projectile"))
+        {
+            enemyObj.ApplyDamage(enemyObj, 1);
+        }
+    }
 }
